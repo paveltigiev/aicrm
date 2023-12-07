862b202e8203c6df1e7bba01e8c2d62a47bc3b65
Pavel Tigiev <paulo.tigiev@gmail.com>
2023-12-07 13:08:06 +0300

    create page

---
+++ src/components/ChatBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-card class="box-card">
+  </el-card>
+</template>
+
+<style lang="scss">
+.box-card {
+  margin-left: 20px;
+  margin-top: 20px;
+  width: 506px;
+  height: 628px;
+  border-radius: 8px;
+  border: 1px solid #3371F2;
+  background: #FFF;
+ }
+</style>
