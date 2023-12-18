5d0a1ada3700fcf3c29b7a4f96e00207ca415dd2
Pavel Tigiev <paulo.tigiev@gmail.com>
2023-12-18 20:23:30 +0300

    fix router

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
 import vue3GoogleLogin from 'vue3-google-login'
 import App from './App.vue'
-import router from './router';
 
-const app = createApp(App);
-app.use(router);
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Landing',
+    component: () => import('./views/LandingPage.vue')
+  }
+];
 
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
 app.use(vue3GoogleLogin, {
   clientId: '???'
 })
-
 app.mount('#app')
