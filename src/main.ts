8aa44a0241d3b593740e78b47580e2e8ec462a96
Pavel Tigiev <paulo.tigiev@gmail.com>
2023-12-18 20:38:09 +0300

    remove router

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
 import vue3GoogleLogin from 'vue3-google-login'
 import App from './App.vue'
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Landing',
-    component: () => import('./views/LandingPage.vue')
-  }
-];
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
-
 const app = createApp(App)
-
-app.use(router)
 app.use(vue3GoogleLogin, {
   clientId: '???'
 })
