ab51f49eb2721eb122e7c82dc514369684cd9804
Pavel Tigiev <paulo.tigiev@gmail.com>
2023-12-18 12:41:16 +0300

    add sidebar animation

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -28,8 +28,8 @@ const telegramCallback = (user: any) => {
 
 <template>
   <Teleport to="body">
-    <div class="sidebar" v-if="visible">
-      <div class="sidebar__overlay" @click="hideSidebar"></div>
+    <div class="overlay" @click="hideSidebar" v-if="visible"></div>
+    <div class="sidebar" :class="[visible ? 'sidebar-visible' : 'sidebar-hidden']">
       <div class="sidebar__close-btn" @click="hideSidebar">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
           <rect x="8" y="9.44009" width="2.03636" height="20.3636" transform="rotate(-45 8 9.44009)" fill="white"/>
@@ -68,35 +68,36 @@ const telegramCallback = (user: any) => {
 </template>
 
 <style lang="scss" scoped>
-.sidebar {
+.overlay {
+  position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.60);
+  z-index: 1;
+
+  @include mq($bp-super-small, $bp-medium-big) {
+    display: none;
+  }
+}
+.sidebar {
   position: fixed;
-  z-index: 999;
-  display: flex;
-  justify-content: flex-end;
+  z-index: 2;
+  right: 0;
+  top: 0;
+  transition: .7s;
 
-  transition: transform 0.3s ease; // Add transition property
-
-  &.sidebar--visible {
-    transform: translateX(0); // Move the sidebar in when visible
+  &-visible {
+    transform: translateX(0);
+  }
+  &-hidden {
+    transform: translateX(500px);
   }
 
-
-  &__overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(0, 0, 0, 0.60);
-    z-index: 0;
-
-    @include mq($bp-super-small, $bp-medium-big) {
-     display: none;
-    }
+  @include mq($bp-super-small, $bp-medium-big) {
+    width: 100vw;
+    padding-top: 193px;
   }
 
   &__close-btn {
@@ -113,9 +114,9 @@ const telegramCallback = (user: any) => {
 
   &__content {
     position: relative;
+    z-index: 1;
     width: 420px;
     height: 100vh;
-    flex-shrink: 0;
     background: #fff;
     padding: 202px 32px;
     text-align: center;
@@ -124,12 +125,6 @@ const telegramCallback = (user: any) => {
     background-repeat: no-repeat;
     background-size: 816px;
     background-position: center top;
-    z-index: 1;
-
-    @include mq($bp-super-small, $bp-medium-big) {
-      width: 100vw;
-      padding-top: 193px;
-    }
 
     &::before {
       content: '';
